ba3203535c9c474996bf4069f09b8a0ad56bc348
matildevmafonso <matildeafonso2006@gmail.com>
2025-11-22 18:42:49 +0000

    initial setup

---
+++ ai-football-analyst/src/index.ts
@@ -0,0 +1,34 @@
+import { fromHono } from "chanfana";
+import { Hono } from "hono";
+import { Ai } from "@cloudflare/ai";
+import { TaskCreate } from "./endpoints/taskCreate";
+import { TaskDelete } from "./endpoints/taskDelete";
+import { TaskFetch } from "./endpoints/taskFetch";
+import { TaskList } from "./endpoints/taskList";
+
+export interface Env {
+	AI: any;
+}
+
+// Start a Hono app
+const app = new Hono<{ Bindings: Env }>();
+
+// Setup OpenAPI registry
+const openapi = fromHono(app, {
+	docs_url: "/",
+});
+
+app.get("/", c => {
+	return c.json("AI Football Analyst is running!");
+});
+// Register OpenAPI endpoints
+openapi.get("/api/tasks", TaskList);
+openapi.post("/api/tasks", TaskCreate);
+openapi.get("/api/tasks/:taskSlug", TaskFetch);
+openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+
+// You may also register routes for non OpenAPI directly on Hono
+// app.get('/test', (c) => c.text('Hono!'))
+
+// Export the Hono app
+export default app;
